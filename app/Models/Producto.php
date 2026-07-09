b04bc818465a94267bbd49697c9532ee40c733f8
Freddy Mendez misena <58695891+freddy-mendez@users.noreply.github.com>
2026-07-09 10:53:38 -0500

    cargar imagen al servidor ej productos

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -3,17 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Producto extends Model
 {
     public $primaryKey = 'producto_id';
-    
+
     public $fillable = [
         'nombre_producto',
         'descripcion',
         'precio_unitario',
         'stock',
-        'iva_porcentaje'
+        'iva_porcentaje',
+        'imagen_path'
     ];
 
     public $timestamps = false;
@@ -21,6 +23,15 @@ class Producto extends Model
     public function facturas()
     {
         return $this->belongsToMany(Factura::class, 'detalle_facturas', 'producto_id', 'factura_id')
-                    ->withPivot('detalle_id', 'cantidad', 'precio_venta', 'subtotal_linea');
+            ->withPivot('detalle_id', 'cantidad', 'precio_venta', 'subtotal_linea');
     }
+
+    public function getImagenUrlAttribute()
+    {
+        $path = $this->imagen_path ?: 'productos/default.png';
+        return Storage::disk('public')->url($path);
+    }
+
+    protected $appends = ['imagen_url'];
+
 }
